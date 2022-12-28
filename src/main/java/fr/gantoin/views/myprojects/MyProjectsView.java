d6ff971e126e9dbfb7860694c1a552b725ec7e55
start.vaadin.com <no-reply@vaadin.com>
2022-12-28 10:53:11 +0000

    Generated project

---
+++ src/main/java/fr/gantoin/views/myprojects/MyProjectsView.java
@@ -0,0 +1,31 @@
+package fr.gantoin.views.myprojects;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import fr.gantoin.views.MainLayout;
+
+@PageTitle("My Projects")
+@Route(value = "projects", layout = MainLayout.class)
+public class MyProjectsView extends VerticalLayout {
+
+    public MyProjectsView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
