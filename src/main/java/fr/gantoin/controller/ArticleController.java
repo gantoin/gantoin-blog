50466e763f9b7a6b24d65b3f0ca3d31219c76a30
gantoin <gantoin@pm.me>
2023-01-05 13:30:47 +0100

    feature: ⏳ execute refreshing gists method every hour

--- src/main/java/fr/gantoin/controller/ArticleController.java
+++ src/main/java/fr/gantoin/controller/ArticleController.java
@@ -2,6 +2,7 @@ package fr.gantoin.controller;
 
 import java.io.IOException;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ public class ArticleController {
     }
 
     @GetMapping("/refresh-gists")
+    @Scheduled(cron = "0 * * * *") // every hour
     public void refreshGists() {
         try {
             articleService.downloadArticles();
