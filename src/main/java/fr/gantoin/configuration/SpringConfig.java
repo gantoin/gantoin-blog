a0758d77cf1422528ebbe8d74b921c9878fad946
gantoin <gantoin@pm.me>
2023-01-05 14:46:15 +0100

    feature: 🌱 add Spring configuration to @EnableScheduling

---
+++ src/main/java/fr/gantoin/configuration/SpringConfig.java
@@ -0,0 +1,10 @@
+package fr.gantoin.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SpringConfig {
+    // Nothing to do here
+}
