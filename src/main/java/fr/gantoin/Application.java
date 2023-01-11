a562f42fd1f113114aa8a4ed8566de7242e0409f
gantoin <gantoin@pm.me>
2023-01-11 17:09:31 +0100

    feature: 📸 add favicon

--- src/main/java/fr/gantoin/Application.java
+++ src/main/java/fr/gantoin/Application.java
@@ -2,6 +2,7 @@ package fr.gantoin;
 
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * and some desktop browsers.
  *
  */
+@PWA(name = "Gantoin's Blog", shortName = "Gantoin's Blog")
 @SpringBootApplication
 @Theme(value = "gantoin-blog")
 @NpmPackage(value = "line-awesome", version = "1.3.0")
